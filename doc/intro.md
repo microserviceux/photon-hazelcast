5a694c6631d31d592234891bb1ba8f4f50059880
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-11-10 12:10:43 +0000

    Move code from photon

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to photon-hazelcast
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject tranchis/photon-hazelcast "0.9.40"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :aot :all
+  :java-source-paths ["java"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [tranchis/photon-db "0.9.31"]
+                 [chazel "0.1.5"]])
